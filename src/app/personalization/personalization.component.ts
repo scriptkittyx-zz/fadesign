77efa794d77f16cc2613afb590c96bb27d82bc14
Nicole Dormido <ndormido@firstam.com>
2019-04-11 15:35:27 -0700

    personalization component created

---
+++ src/app/personalization/personalization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personalization',
+  templateUrl: './personalization.component.html',
+  styleUrls: ['./personalization.component.css']
+})
+export class PersonalizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
