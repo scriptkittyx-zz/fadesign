460396c8ad6a9eb2e5ffe85355e03bd7fee18452
Nicole Dormido <ndormido@firstam.com>
2019-04-11 15:34:09 -0700

    added new components to routing module ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FormsComponent } from './forms/forms.component';
 import { GridComponent } from './grid/grid.component';
+import { ElementsComponent } from './elements/elements.component';
+import { PersonalizationComponent } from './personalization/personalization.component';
+import { SystemNav1Component } from './systemnav1/systemnav1.component';
+import { SystemNav2Component } from './systemnav2/systemnav2.component';
+
+
 
 const routes: Routes = [
     {
@@ -17,6 +23,22 @@ const routes: Routes = [
     {
         path: 'grid',
         component: GridComponent
+    },
+    {
+        path: 'elements',
+        component: ElementsComponent
+    },
+    {
+        path: 'personalization',
+        component: PersonalizationComponent
+    },
+    {
+        path: 'systemnav1',
+        component: SystemNav1Component
+    },
+    {
+        path: 'systemnav2',
+        component: SystemNav2Component
     }
 ];
 
