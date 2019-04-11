9d1e26fb7196b4f94e40d6da88c5467723193359
Nicole Dormido <ndormido@firstam.com>
2019-04-11 15:35:53 -0700

    system nav 2 component created

---
+++ src/app/systemnav2/systemnav2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-systemnav2',
+  templateUrl: './systemnav2.component.html',
+  styleUrls: ['./systemnav2.component.css']
+})
+export class SystemNav2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
