892c01c1ba2fba8e2d4e041e97fa842906842f44
Nicole Dormido <ndormido@firstam.com>
2019-04-11 15:35:39 -0700

    system nav 1 component created

---
+++ src/app/systemnav1/systemnav1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-systemnav1',
+  templateUrl: './systemnav1.component.html',
+  styleUrls: ['./systemnav1.component.css']
+})
+export class SystemNav1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
